282d2e634d47478f8b2e493743506dff59681ee0
Dutches360 <60424749+Dutches360@users.noreply.github.com>
2020-03-05 20:21:13 +0200

    done

--- src/app/slider/script.js
+++ src/app/slider/script.js
@@ -1,45 +1,45 @@
-const slides=document.querySelector(".slider").children;
-const prev=document.querySelector(".prev");
-const next=document.querySelector(".next");
-let index=0;
+// const slides=document.querySelector(".slider").children;
+// const prev=document.querySelector(".prev");
+// const next=document.querySelector(".next");
+// let index=0;
 
-prev.addEventListener("click",function(){
-    prevSlide()
-})
+// prev.addEventListener("click",function(){
+//     prevSlide()
+// })
 
-next.addEventListener("click",function(){
-    nextSlide()
-})
+// next.addEventListener("click",function(){
+//     nextSlide()
+// })
 
 
-function circleIndicator(){
-    for(let i=0; i< slides.length; i++){
-        const div=document.createElement("div");
-        div.innerHTML=i+1;
-        div.setAttribute("onclick","indicateSlide(this)")
-        indicator.appendChild(div);
-    }
-}
-
-circleIndicator();
-function prevSlide(){
-    if(index==0){
-        index=slides.length-1;
-    }
-    else{
-        index--;
-    }
-}
-
-// function nextSlide(){
-//     if(index==slides.length-1){
-//         index=0;
+// function circleIndicator(){
+//     for(let i=0; i< slides.length; i++){
+//         const div=document.createElement("div");
+//         div.innerHTML=i+1;
+//         div.setAttribute("onclick","indicateSlide(this)")
+//         indicator.appendChild(div);
 //     }
-// else{ 
-//     index++;
 // }
-// chan
+
+// circleIndicator();
+// function prevSlide(){
+//     if(index==0){
+//         index=slides.length-1;
+//     }
+//     else{
+//         index--;
+//     }
 // }
-// function changeSlide(){
-//     slides[index].classList.add("active")
-// }
+
+//  function nextSlide(){
+//      if(index==slides.length-1){
+//          index=0;
+//      }
+//      else{ 
+//         index++;
+//      }
+// // chan
+//  }
+//  function changeSlide(){
+//      slides[index].classList.add("active")
+//  }
